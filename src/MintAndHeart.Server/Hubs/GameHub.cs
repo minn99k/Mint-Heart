2473fda4f995ec07b52ef46f56b3b11171c02057
pingguG <stargate1327@naver.com>
2026-03-28 17:50:58 +0900

    Improve SignalR integration and optimize GameHub methods

--- src/MintAndHeart.Server/Hubs/GameHub.cs
+++ src/MintAndHeart.Server/Hubs/GameHub.cs
@@ -5,6 +5,10 @@ namespace MintAndHeart.Server.Hubs
     // This class is the 'channel' that the client and server can communicate through
     public class GameHub : Hub
     {
+        public async Task Ping(){
+            Console.WriteLine($"Ping received from: {Context.ConnectionId}");
+            await Clients.Caller.SendAsync("Pong", "Responed Messeage from Server!");
+        }
         // Leave it empty for now!
         // TODO: Add functions like "create room", "send units" later.
     }
