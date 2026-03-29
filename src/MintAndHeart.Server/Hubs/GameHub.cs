0b57e5e579b1bfa56b8d18e5e50d697e2462a28f
pingguG <stargate1327@naver.com>
2026-03-29 16:07:56 +0900

    의존성 주입 roomservice

--- src/MintAndHeart.Server/Hubs/GameHub.cs
+++ src/MintAndHeart.Server/Hubs/GameHub.cs
@@ -1,13 +1,35 @@
 using Microsoft.AspNetCore.SignalR;
+using MintAndHeart.Server.Services;
 
 namespace MintAndHeart.Server.Hubs
 {
     // This class is the 'channel' that the client and server can communicate through
     public class GameHub : Hub
     {
+        private readonly RoomService _roomService;
+
+        public GameHub(RoomService roomService)
+        {
+            _roomService = roomService;
+        }
+        public override async Task OnConnectedAsync()
+        {
+            Console.WriteLine($"Player connected: {Context.ConnectionId}");
+            await base.OnConnectedAsync();
+            // Call the base method to ensure proper setup
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            Console.WriteLine($"Player disconnected: {Context.ConnectionId}");
+            await base.OnDisconnectedAsync(exception); 
+        }
+
         public async Task Ping(){
-            Console.WriteLine($"Ping received from: {Context.ConnectionId}");
-            await Clients.Caller.SendAsync("Pong", "Responed Messeage from Server!");
+            var message = _roomService.TestConnection();
+            // Log the received message and the connection ID of the sender
+            Console.WriteLine($"Received Ping from {Context.ConnectionId}");
+            await Clients.Caller.SendAsync("Pong", message);
             // Clients.Caller: 이 메서드를 호출한 클라이언트에게만 응답
         }
         // Leave it empty for now!
